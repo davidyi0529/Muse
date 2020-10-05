93ed7f791962ed149d797c1c131533538c1cb6dd
Jerry Leong <jerry.leong20@gmail.com>
2020-10-05 00:20:56 -0700

    add features to modals

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
     $("#searchBtn").on("click", function(e){
         e.preventDefault();
         //test console.log to check if click works
-        console.log("click")
+        //console.log("click")
 
         //Genius API
         var APIKey = "14S5okXH4GKYe-PKaBH0HlfjVuSDyaK9ZUwz3ep3rqUAc37eQtPWr9j6NLxyCBp8";
@@ -119,9 +119,8 @@ $(document).ready(function(){
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
-                //console.log(response);
+                console.log(response);
                 var hitsLength = response.response.hits.length;
-                console.log(hitsLength);
                 for(var i = 0; i < hitsLength; i++){
                     //list search results
                     var hitsTitle = $("<button>");
@@ -139,6 +138,16 @@ $(document).ready(function(){
                     var hitsModalTitle = $("<div class='uk-modal-title'></div>");
                     hitsModalTitle.text(response.response.hits[i].result.full_title);
                     hitsModalBody.append(hitsModalTitle);
+                    var hitsImg = $("<img>");
+                    hitsImg.attr("src", response.response.hits[i].result.song_art_image_thumbnail_url);
+                    hitsModalTitle.append(hitsImg);
+                    var hitsLyric = $("<a>");
+                    var hitsModalButtons = $("<p class='uk-text-right'></p>");
+                    var hitsModalClose = $("<button class='uk-button uk-modal-close' type='button'>Close</button>");
+                    hitsModalButtons.append(hitsModalClose);
+                    hitsModalTitle.append(hitsModalButtons);
+                    var hitsModalPlay = $("<button id ='play' class='uk-button' type='button'>Play</button>");
+                    hitsModalButtons.append(hitsModalPlay);
 
                     $("#modals").append(hitsModal);
                     hitsTitle.attr("uk-toggle","target: #modal"+i);
@@ -158,7 +167,7 @@ $(document).ready(function(){
                 url: queryURL,
                 method: "GET"
             }).then(function(response) {
-                //console.log(response);
+                console.log(response);
                 var hitsLength = response.response.hits.length;
                 console.log(hitsLength);
                 for(var i = 0; i < hitsLength; i++){
@@ -178,10 +187,54 @@ $(document).ready(function(){
                     var lyricModalTitle = $("<div class='uk-modal-title'></div>");
                     lyricModalTitle.text(response.response.hits[i].result.full_title);
                     lyricModalBody.append(lyricModalTitle);
+                    var lyricImg = $("<img>");
+                    lyricImg.attr("src", response.response.hits[i].result.song_art_image_thumbnail_url);
+                    lyricModalTitle.append(lyricImg);
+
 
                     $("#modals").append(lyricModal);
                     lyricTitle.attr("uk-toggle","target: #modal"+i);
+
+                    //genius api
+                    //var songId = response.response.hits[i].result.api_path;
+                    //console.log(songId);
+                    // var settings = {
+                    //     "async": true,
+                    //     "crossDomain": true,
+                    //     "url": "https://genius.p.rapidapi.com"+songId,
+                    //     "method": "GET",
+                    //     "headers": {
+                    //         "x-rapidapi-host": "genius.p.rapidapi.com",
+                    //         "x-rapidapi-key": "b5a19d4784msh263354a8d69856ep11fb51jsncd8286e67c37"
+                    //     }
+                    // }
+            
+                    // $.ajax(settings).done(function (response) {
+                    //     console.log(response);
+                    //     console.log(response.response.song.apple_music_player_url);
+                    //     var musicPlayer = $("<iframe>");
+                    //     musicPlayer.attr("src", response.response.song.apple_music_player_url);
+                    //     lyricModalTitle.append(musicPlayer);
+                        
+                    // });
                 }
+                // var settings = {
+                //     "async": true,
+                //     "crossDomain": true,
+                //     "url": "https://genius.p.rapidapi.com/songs/3315890",
+                //     "method": "GET",
+                //     "headers": {
+                //         "x-rapidapi-host": "genius.p.rapidapi.com",
+                //         "x-rapidapi-key": "b5a19d4784msh263354a8d69856ep11fb51jsncd8286e67c37"
+                //     }
+                // }
+        
+                // $.ajax(settings).done(function (response) {
+                //     console.log(response);
+                //     console.log(response.response.song.media[0].url);
+                  
+                //     //$("#music")
+                // });
             });
         }
         //error if no search option picked
@@ -191,23 +244,23 @@ $(document).ready(function(){
 
         //===============================================================================
         //Rapid Genius API
-        //var settings = {
-            //"async": true,
-            //"crossDomain": true,
-            //"url": "https://genius.p.rapidapi.com/songs/3315890",
-            //"method": "GET",
-            //"headers": {
-                //"x-rapidapi-host": "genius.p.rapidapi.com",
-                //"x-rapidapi-key": "b5a19d4784msh263354a8d69856ep11fb51jsncd8286e67c37"
-            //}
-        //}
+        // var settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://genius.p.rapidapi.com/songs/3315890",
+        //     "method": "GET",
+        //     "headers": {
+        //         "x-rapidapi-host": "genius.p.rapidapi.com",
+        //         "x-rapidapi-key": "b5a19d4784msh263354a8d69856ep11fb51jsncd8286e67c37"
+        //     }
+        // }
 
-        //$.ajax(settings).done(function (response) {
-            //console.log(response);
-            //console.log(response.response.song.media[0].url);
+        // $.ajax(settings).done(function (response) {
+        //     console.log(response);
+        //     console.log(response.response.song.media[0].url);
           
-            //$("#music")
-        //});
+        //     $("#music")
+        // });
 
         //===================================================================================
         // Musix API
